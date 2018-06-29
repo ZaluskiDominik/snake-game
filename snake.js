135b7fe9f6f7c344daf6b4a2380c00a8776dcfc1
Taisa <d.zaluski98@gmail.com>
2018-06-29 21:01:41 +0100

    Changed graphical style of webpage

--- snake.js
+++ snake.js
@@ -23,20 +23,19 @@ let gameStarted;
 
 let score;
 
-//canvas handle and 2d context
+//handles for canvas and 2d context
 let canvas, ctx;
 
-function initCanvas()
+(function initCanvas()
 {
 	canvas=document.getElementById("canvas");
 	ctx=canvas.getContext("2d");
 	//set size of the canvas
 	canvas.width=(fieldSize + separatorWidth) * w - separatorWidth;
 	canvas.height=(fieldSize + separatorWidth) * h - separatorWidth;
-	
-	document.getElementById("menu").style.width=canvas.width + "px";
-}
+})();
 
+//increment the value of player's score
 function updateScore()
 {
 	document.getElementById("score").innerHTML="Score: " + (++score);
@@ -48,12 +47,12 @@ let SnakeBody=function(x, y, color)
 	this.x=x;
 	this.y=y;
 	this.color=color;
+}
 
-	this.draw=function()
-	{
-		ctx.fillStyle=this.color;
-		ctx.fillRect(this.x * (fieldSize + separatorWidth), this.y * (fieldSize + separatorWidth), fieldSize, fieldSize);
-	}
+SnakeBody.prototype.draw=function()
+{
+	ctx.fillStyle=this.color;
+	ctx.fillRect(this.x * (fieldSize + separatorWidth), this.y * (fieldSize + separatorWidth), fieldSize, fieldSize);
 }
 
 //array of snake's pieces of body
@@ -84,20 +83,10 @@ function randFood()
 	while (i!=snake.length);
 }
 
-//init game variables
-function initGame()
+//set initial value to snake's current direction vector and previous direction vector
+function initDirectionVectors()
 {
-	clearInterval(timer);
-	notMovingPeriod=100;
-	gameStarted=false;
-	score=-1;
-
-	updateScore();
-	initSnake(7);
-	randFood();
-	drawGame();
-	drawMessage("Press any key to start...");
-
+	//snake goes up
 	prevDirectionVector={
 		x: 0,
 		y: -1
@@ -108,6 +97,27 @@ function initGame()
 	}
 }
 
+//init game variables
+function initGame()
+{
+	//clear timer from previous game
+	clearInterval(timer);
+	notMovingPeriod=100;
+	gameStarted=false;
+	//reset score
+	score=-1;
+	updateScore();
+
+	initDirectionVectors();
+	initSnake(7);
+	randFood();
+	drawGame();
+	drawMessage("Press 'enter' to start...");
+	console.log("init");
+}
+
+initGame();
+
 //drawing functions
 //************************************************************************
 
@@ -117,42 +127,51 @@ function clearCanvas()
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+//draw lines that separates grid cells
 function drawSeparators()
 {
 	ctx.lineWidth=separatorWidth;
 	ctx.fillStyle="gray";
 	//vericall
 	for (let i=1 ; i<w ; i++)
-		ctx.fillRect(i * (fieldSize + separatorWidth), 0, separatorWidth, canvas.height);
+		ctx.fillRect(i * (fieldSize + separatorWidth) - 1, 0, separatorWidth, canvas.height);
 
 	//horizontall
 	for (let i=1 ; i<h ; i++)
-		ctx.fillRect(0, i * (fieldSize + separatorWidth), canvas.width, separatorWidth);
+		ctx.fillRect(0, i * (fieldSize + separatorWidth) - 1, canvas.width, separatorWidth);
 }
 
 function drawFood()
 {
 	ctx.fillStyle="yellow";
 	ctx.beginPath();
-	ctx.arc(food.x * (fieldSize + separatorWidth) + (fieldSize/2), food.y * (fieldSize + separatorWidth) + (fieldSize/2), fieldSize/2, 0, Math.PI*2);
+	ctx.arc(food.x * (fieldSize + separatorWidth) + (fieldSize/2), 
+		food.y * (fieldSize + separatorWidth) + (fieldSize/2), fieldSize/2, 0, Math.PI*2);
 	ctx.fill();
 }
 
 //draw text
 function drawMessage(msg)
 {
+	ctx.save();
 	ctx.font = "36px Arial";
 	ctx.fillStyle="blue";
 	ctx.textAlign="center";
+	ctx.shadowColor="black";
+	ctx.shadowBlur=8;
+	ctx.globalAlpha=0.7;
 	ctx.fillText(msg, canvas.width/2, canvas.height/2);
+	ctx.restore();
 }
 
+//draw snake onto canvas
 function drawSnake()
 {
 	for (let i=0 ; i<snake.length ; i++)
 		snake[i].draw();
 }
 
+//call all defined drawing functions
 function drawGame()
 {
 	clearCanvas();
@@ -216,8 +235,8 @@ function moveSnake()
 	drawGame();
 }
 
-//keyboard events
-window.addEventListener("keydown", function(event)
+//handle player's keys(up, down, left, right)
+function onKeyPressed(event)
 {
 	switch (event.keyCode)
 	{
@@ -254,17 +273,22 @@ window.addEventListener("keydown", function(event)
 		}
 		break;
 	}
+}
 
-	//start game on pressing any key
-	if (!gameStarted)
+function handleStartGameKeyPressed(event)
+{
+	//start game when player press enter
+	if ( (!gameStarted) && event.keyCode==13 )
 	{
 		gameStarted=true;
 		//start counting
 		timer=setInterval(moveSnake, notMovingPeriod);
 	}
 }
-)
 
-//invoke startup functions
-initCanvas();
-initGame();
+//keyboard events
+window.addEventListener("keydown", function(event)
+{
+	onKeyPressed(event);
+	handleStartGameKeyPressed(event);
+});
